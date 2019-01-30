d6deacd757ce0a195ccac31382c3f39bec9f70ca
NnekaLy <p17210378@my365.dmu.ac.uk>
2019-01-30 12:39:51 +0000

    Created clsAppointment + test class

---
+++ Test Framework/tstAppointment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Class_Library;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test_Framework
+{
+    [TestClass]
+    public class tstAppointment
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            // create an instance of our class clsAppointment
+            clsAppointment AnAppointment = new clsAppointment();
+            // check to see that the class is not null
+            Assert.IsNotNull(AnAppointment);
+        }
+    }
+}
