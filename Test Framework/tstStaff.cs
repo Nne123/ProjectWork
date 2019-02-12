4fc416d03b55ea134e241e06200a30c799f0d7ff
shefika <p17204160@my365.dmu.ac.uk>
2019-02-12 22:29:46 +0000

    Creation of clsStaff + tst class

---
+++ Test Framework/tstStaff.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Class_Library;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test_Framework
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of our class clsStaff
+            clsStaff AnStaff = new clsStaff();
+            //check to see that the class is not null
+            Assert.IsNotNull(AnStaff);
+        }
+    }
+}
