996fc655d2f94281faaac83c0b6068004b5613bb
shefika <p17204160@my365.dmu.ac.uk>
2019-02-12 23:56:59 +0000

    Added to my clsStaff, First & Last name string.

--- Class Library/clsStaff.cs
+++ Class Library/clsStaff.cs
@@ -2,5 +2,40 @@
 {
     public class clsStaff
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Valid(string FirstName, string LastName)
+        {
+            string Error = "";
+            if (LastName.ToString().Length == 15 || LastName.ToString().Length == 0)
+            {
+                Error = "";
+            }
+            if (LastName.ToString().Length> 6 || LastName.ToString().Length < 6)
+            {
+                Error = "LasttName must be 6 letters long";
+            }
+            //if firstName is blank
+            else if (FirstName.Length == 0)
+            {
+                //return an error message
+                Error = "First Name cant be blank";
+            }
+            //if FirstName is less than 4 characters long, display error message
+            else if (FirstName.Length < 4)
+            {
+                //display an error message
+                Error = "First Name must be at least 4 characters long or more";
+            }
+            //if first name is more than 50 characters display an error message
+            else if (FirstName.Length > 50)
+            {
+                //display an error message
+                Error = "First Name can't have 50 characters, too long.";
+            }
+            //return the results
+            return Error;
+        }
     }
 }
