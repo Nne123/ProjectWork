2f7e250d1a5c8efb7858f7dc2baad4f4beaabb77
NnekaLy <p17210378@my365.dmu.ac.uk>
2019-02-13 12:29:01 +0000

    New ValidMethodOK test + Condition statement

--- Class Library/clsAppointment.cs
+++ Class Library/clsAppointment.cs
@@ -1,8 +1,25 @@
-﻿namespace Class_Library
+﻿using System;
+
+namespace Class_Library
 {
     public class clsAppointment
     {
         public string CarRegNo { get; set; }
         public int AppointmentID { get; set; }
+
+        public string Valid(string carRegNo)
+        {
+            // if the carRegNo is not blank
+            if (carRegNo != "")
+            {
+                // return a blank string
+                return "";
+            }
+            else
+            {
+                // else return an error message
+                return "The Car Reg No may not be blank!";
+            }
+        }
     }
 }
--- Test Framework/tstAppointment.cs
+++ Test Framework/tstAppointment.cs
@@ -41,5 +41,35 @@ namespace Test_Framework
             // test to see that the two values are the same
             Assert.AreEqual(AnAppointment.CarRegNo, SomeCarRegNo);
         }
+
+        [TestMethod]
+        public void ValidMethodOK()
+        {
+            // create an instance of clsAppointment
+            clsAppointment AnAppointment = new clsAppointment();
+            // create a string variable to store the result of the validation
+            String Error = "";
+            // create some test data to test the method
+            string CarRegNo = "123 G6H";
+            // invoke the method
+            Error = AnAppointment.Valid(CarRegNo);
+            // test to see that the result is OK i.e. there was no error message returned
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void CarRegNoMinLessOne()
+        {
+            // create an instance of clsAppointment
+            clsAppointment AnAppointment = new clsAppointment();
+            // create a string variable to store the result of the validation
+            String Error = "";
+            // create some test data to test the method
+            string CarRegNo = "";
+            // invoke the method
+            Error = AnAppointment.Valid(CarRegNo);
+            //test to see that the result is NOT OK i.e there should be an error message
+            Assert.AreNotEqual(Error, "");
+        }
     }
 }
