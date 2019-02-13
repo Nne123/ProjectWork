518c505b88965fdc14ada7be69f16178cb016d6a
P17204160 <P17204160@GWHXX282WIN010W>
2019-02-13 11:28:39 +0000

    Added new tests to tstStaff, like StaffID and LastName

--- Class Library/clsStaff.cs
+++ Class Library/clsStaff.cs
@@ -4,6 +4,9 @@
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string PhoneNo { get; set; }
+        public string Email { get; set; }
+        public string StaffID { get; set; }
 
         public string Valid(string FirstName, string LastName)
         {
@@ -12,9 +15,9 @@
             {
                 Error = "";
             }
-            if (LastName.ToString().Length > 6 || LastName.ToString().Length < 6)
+            if (LastName.ToString().Length > 4 || LastName.ToString().Length < 4)
             {
-                Error = "LasttName must be 6 letters long";
+                Error = "LasttName must be 4 letters long";
             }
             //if firstName is blank
             else if (FirstName.Length == 0)
@@ -44,10 +47,12 @@
             {
                 Error = "";
             }
-            if (LastName.ToString().Length > 6 || LastName.ToString().Length < 6)
+            if (LastName.ToString().Length > 4 || LastName.ToString().Length < 4)
             {
-                Error = "LasttName must be 6 letters long";
+                Error = "LasttName must be 4 letters long";
             }
+
+
             //if firstName is blank
             else if (FirstName.Length == 0)
             {
@@ -67,11 +72,67 @@
                 Error = "First Name can't have 50 characters, too long.";
             }
 
+
+            else if (PhoneNo.Length == 0)
+            {
+                //return an error message
+                Error = "PhoneNo cant be blank";
+            }
+            //if FirstName is less than 4 characters long, display error message
+            else if (PhoneNo.Length < 4)
+            {
+                //display an error message
+                Error = "PhoneNo must be at least 4 characters long or more";
+            }
+            //if first name is more than 50 characters display an error message
+            else if (PhoneNo.Length > 50)
+            {
+                //display an error message
+                Error = "PhoneNo can't have 50 characters, too long.";
+            }
+
+
+            else if (Email.Length == 0)
+            {
+                //return an error message
+                Error = "Email can't be blank";
+            }
+            //if Email is less than 4 characters long, display error message
+            else if (Email.Length < 4)
+            {
+                //display an error message
+                Error = "Email must be at least 4 characters long or more";
+            }
+            //if Email is more than 50 characters display an error message
+            else if (Email.Length > 50)
+            {
+                //display an error message
+                Error = "Email can't have 50 characters, too long.";
+            }
             else if (Search.ToString().Length == 0)
             {
                 Error = "The search field has to contain something";
             }
+
+            else if (StaffID.Length == 0)
+            {
+                //return an error message
+                Error = "StaffID cant be blank";
+            }
+            //if StaffID is less than 4 characters long, display error message
+            else if (StaffID.Length < 6)
+            {
+                //display an error message
+                Error = "StaffID must be at least 6 characters long or more";
+            }
+            //if first name is more than 50 characters display an error message
+            else if (StaffID.Length > 50)
+            {
+                //display an error message
+                Error = "StaffID can't have 50 characters, too long.";
+            }
             //return the results
+            //
             return Error;
         }
 
