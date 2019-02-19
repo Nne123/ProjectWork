1cc4730b6cb30c584e2ab59e7fdb35eab384ca7c
Shefika <p17204160@my365.dmu.ac.uk>
2019-02-19 20:48:55 +0000

    Made a clsStaffCollection + tstStaffCollection

---
+++ Class Library/clsStaffCollection.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Class_Library
+{
+    public class clsStaffCollection
+    {
+        public List<clsStaff> StaffList { get; set; }
+        public int Count { get; set; }
+        public clsStaff ThisStaff { get; set; }
+    }
+}
---
+++ Test Framework/tstStaffCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Class_Library;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test_Framework
+{
+    [TestClass]
+    public class tstStaffCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class i want to create
+            clsStaffCollection AllStaffs = new clsStaffCollection();
+            //test to see if it exists
+            Assert.IsNotNull(AllStaffs);
+        }
+    }
+}
