59c93c7e63b8adf64b68e11faa647d52ed1cf610
NnekaLy <p17210378@my365.dmu.ac.uk>
2019-02-25 19:22:48 +0000

    Tests added appointments and mot time

--- Class Library/clsAppointment.cs
+++ Class Library/clsAppointment.cs
@@ -4,14 +4,120 @@ namespace Class_Library
 {
     public class clsAppointment
     {
-        public string CarRegNo { get; set; }
-        public int AppointmentID { get; set; }
-        public DateTime MOTDate { get; set; }
-        public int JobID { get; set; }
-        public int CustomerID { get; set; }
-        public int StaffID { get; set; }
-        public int MOTTimeID { get; set; }
-        public bool Active { get; set; }
+        // private data member for the the properties
+        private Int32 mAppointmentID;
+        private string mCarRegNo;
+        private Int32 mCustomerID;
+        private Int32 mStaffID;
+        private Int32 mJobID;
+        private DateTime mMOTDate;
+        private Int32 mMOTTimeID;
+        private Boolean mActive;
+
+        public string CarRegNo
+        {
+            get
+            {
+                // return the private data
+                return mCarRegNo;
+            }
+            set
+            {
+                // set the value of the private data member
+                mCarRegNo = value;
+            }
+        }
+        public int AppointmentID
+        {
+            get
+            {
+                // return the private data
+                return mAppointmentID;
+            }
+            set
+            {
+                // set the value of the private data member
+                mAppointmentID = value;
+            }
+        }
+        public DateTime MOTDate
+        {
+            get
+            {
+                // return the private data
+                return mMOTDate;
+            }
+            set
+            {
+                // set the value of the private data member
+                mMOTDate = value;
+            }
+        }
+        public int JobID
+        {
+            get
+            {
+                // return the private data
+                return mJobID;
+            }
+            set
+            {
+                // set the value of the private data member
+                mJobID = value;
+            }
+        }
+        public int CustomerID
+        {
+            get
+            {
+                // return the private data
+                return mCustomerID;
+            }
+            set
+            {
+                // set the value of the private data member
+                mCustomerID = value;
+            }
+        }
+        public int StaffID
+        {
+            get
+            {
+                // return the private data
+                return mStaffID;
+            }
+            set
+            {
+                // set the value of the private data member
+                mStaffID = value;
+            }
+        }
+        public int MOTTimeID
+        {
+            get
+            {
+                // return the private data
+                return mMOTTimeID;
+            }
+            set
+            {
+                // set the value of the private data member
+                mMOTTimeID = value;
+            }
+        }
+        public bool Active
+        {
+            get
+            {
+                // return the private data
+                return mActive;
+            }
+            set
+            {
+                // set the value of the private data member
+                mActive = value;
+            }
+        }
 
         public string Valid(string carRegNo, string mOTDate, string CustomerID, string StaffID, string JobID)
         {
@@ -124,6 +230,21 @@ namespace Class_Library
             }
 
             return Error;
+        }
+
+        public bool Find(int AppointmentID)
+        {
+            // set the private data member for the AppointmentID property
+            mAppointmentID = 3;
+            mCarRegNo = "4352 7GF";
+            mCustomerID = 34;
+            mStaffID = 2;
+            mJobID = 6;
+            mMOTDate = Convert.ToDateTime("26/03/2019");
+            mMOTTimeID = 13;
+            mActive = true;
+            // always return true
+            return true;
         }
     }
 }
