a1e2ca3af384c8f60bcaaa691017ab3a35a7fd24
NnekaLy <p17210378@my365.dmu.ac.uk>
2019-02-26 17:11:34 +0000

    Code added to Default Appointment page

--- PBFrontEnd/DefaultAppointment.aspx
+++ PBFrontEnd/DefaultAppointment.aspx
@@ -1,5 +1,5 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/EasyFixBank.Master" AutoEventWireup="true" CodeBehind="DefaultAppointment.aspx.cs" Inherits="PBFrontEnd.Default" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <p>
+    <p style="height: 319px">
         Some tex</p>
 </asp:Content>
--- PBFrontEnd/DefaultAppointment.aspx.cs
+++ PBFrontEnd/DefaultAppointment.aspx.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
+using System.Web.UI.WebControls; 
 
 namespace PBFrontEnd
 {
@@ -23,6 +23,8 @@ namespace PBFrontEnd
         void DisplayAppointments()
         {
             // create an instance of the Appointment collection
+            Class_Library.clsAppointmentCollection Appointments = new Class_Library.clsAppointmentCollection();
+            // set the data source to the list of appointments in the collection
             
         }
     }
