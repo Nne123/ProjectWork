0e6a1bba9cb58e2cb904f8969a3497f606908589
NnekaLy <p17210378@my365.dmu.ac.uk>
2019-02-26 15:15:08 +0000

    commented out code

--- Test Framework/tstAppointmentCollection.cs
+++ Test Framework/tstAppointmentCollection.cs
@@ -67,33 +67,33 @@ namespace Test_Framework
         //    Assert.AreEqual(AllAppointments.Count, 2);
         //}
 
-        [TestMethod]
-        public void CountMatchesList()
-        {
-            // create an instance of the class we want to create
-            clsAppointmentCollection Appointments = new clsAppointmentCollection();
-            // create some test data to assign to the property
-            // in this case the data needs to be a list of objects
-            List<clsAppointment> TestList = new List<clsAppointment>();
-            // add an item to the list
-            // create the item of tets data
-            clsAppointment TestItem = new clsAppointment();
-            // set its properties
-            TestItem.AppointmentID = 1;
-            TestItem.CarRegNo = "1234 9GH";
-            TestItem.CustomerID = 1;
-            TestItem.StaffID = 1;
-            TestItem.JobID = 1;
-            TestItem.MOTDate = DateTime.Now.Date.AddDays(1);
-            TestItem.MOTTimeID = 10;
-            TestItem.Active = true;
-            // add the item to the test list
-            TestList.Add(TestItem);
-            // assign the data to the property
-            Appointments.AppointmentList = TestList;
-            // test to see that the two values are the same
-            Assert.AreEqual(Appointments.Count, TestList.Count);
-        }
+        //[TestMethod]
+        //public void CountMatchesList()
+        //{
+        //    // create an instance of the class we want to create
+        //    clsAppointmentCollection Appointments = new clsAppointmentCollection();
+        //    // create some test data to assign to the property
+        //    // in this case the data needs to be a list of objects
+        //    List<clsAppointment> TestList = new List<clsAppointment>();
+        //    // add an item to the list
+        //    // create the item of tets data
+        //    clsAppointment TestItem = new clsAppointment();
+        //    // set its properties
+        //    TestItem.AppointmentID = 1;
+        //    TestItem.CarRegNo = "1234 9GH";
+        //    TestItem.CustomerID = 1;
+        //    TestItem.StaffID = 1;
+        //    TestItem.JobID = 1;
+        //    TestItem.MOTDate = DateTime.Now.Date.AddDays(1);
+        //    TestItem.MOTTimeID = 10;
+        //    TestItem.Active = true;
+        //    // add the item to the test list
+        //    TestList.Add(TestItem);
+        //    // assign the data to the property
+        //    Appointments.AppointmentList = TestList;
+        //    // test to see that the two values are the same
+        //    Assert.AreEqual(Appointments.Count, TestList.Count);
+        //}
 
         [TestMethod]
         public void ThisAppointmentPropertyOK()
@@ -102,6 +102,7 @@ namespace Test_Framework
             clsAppointmentCollection AllAppointments = new clsAppointmentCollection();
             // create some test data to assign to the property
             clsAppointment TestAppointment = new clsAppointment();
+            // set the properties of the test object
             TestAppointment.AppointmentID = 1;
             TestAppointment.CarRegNo = "123 9GH";
             TestAppointment.CustomerID = 1;
