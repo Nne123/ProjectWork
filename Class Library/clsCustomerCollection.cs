a75539307e528ffc651a44c28bf58ac7a890f065
Tomas <P17208375@my365.dmu.ac.uk>
2019-03-03 11:29:54 +0200

    tstCustomerCollection testing.

--- Class Library/clsCustomerCollection.cs
+++ Class Library/clsCustomerCollection.cs
@@ -1,7 +1,39 @@
-﻿namespace Class_Library
+﻿using System.Collections.Generic;
+
+namespace Class_Library
 {
     public class clsCustomerCollection
     {
-        public int Count { get; set; }
+        public clsCustomer ThisCustomer { get; set; }
+        // Private data member for the list
+        List<clsCustomer> mCustomerList = new List<clsCustomer>();
+        // Public property for the customer list
+        public List<clsCustomer> CustomerList
+        {
+            get
+            {
+                // Return the private data
+                return mCustomerList;
+            }
+            set
+            {
+                // Set the private data
+                mCustomerList = value;
+            }
+        }
+
+        // Public property for count
+        public int Count
+        {
+            get
+            {
+                // Return the count of the list
+                return mCustomerList.Count;
+            }
+            set
+            {
+                // later
+            }
+        }
     }
 }
--- Test Framework/tstCustomerCollection.cs
+++ Test Framework/tstCustomerCollection.cs
@@ -1,15 +1,13 @@
 ﻿using System;
 using Class_Library;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace Test_Framework
 {
     [TestClass]
     public class tstCustomerCollection
     {
-        // New testing variable TestCount with some test data
-        Int32 TestCount = 10;
-
         [TestMethod]
         public void InstanceOK()
         {
@@ -23,11 +21,87 @@ namespace Test_Framework
         public void CountOK()
         {
             // New instance of clsCustomerCollection class
-            clsCustomerCollection Customers = new clsCustomerCollection();
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            // Variable SomeCount with some test data
+            Int32 SomeCount = 0;
             // Assign the test variable into the real one
-            Customers.Count = TestCount;
+            AllCustomers.Count = SomeCount;
             // Check whether they match
-            Assert.AreEqual(Customers.Count, TestCount);
+            Assert.AreEqual(AllCustomers.Count, SomeCount);
+        }
+
+        [TestMethod]
+        public void ThisCustomerOK()
+        {
+            // New instance of clsCustomerCollection class
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            // New instance of clsCustomer class to create the item of test data
+            clsCustomer TestCustomer = new clsCustomer();
+            // Set its properties
+            TestCustomer.AddressLine1 = "Grange 51a";
+            TestCustomer.AddressLine2 = "Leicester";
+            TestCustomer.CustomerID = 1;
+            TestCustomer.Email = "test@test.com";
+            TestCustomer.FirstName = "John";
+            TestCustomer.LastName = "Smith";
+            TestCustomer.PhoneNo = "+444656446641";
+            TestCustomer.CarRegNo = "AF3E4E";
+            // Assign the test variable into the real one
+            AllCustomers.ThisCustomer = TestCustomer;
+            // Check whether they match
+            Assert.AreEqual(AllCustomers.ThisCustomer, TestCustomer);
+        }
+
+        [TestMethod]
+        public void CustomerListOK()
+        {
+            // New instance of clsCustomerCollection class
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            // Create a list named TestList
+            List<clsCustomer> TestList = new List<clsCustomer>();
+            // New instance of clsCustomer class to create the item of test data
+            clsCustomer TestItem = new clsCustomer();
+            // Set its properties
+            TestItem.AddressLine1 = "Grange 51a";
+            TestItem.AddressLine2 = "Leicester";
+            TestItem.CustomerID = 1;
+            TestItem.Email = "test@test.com";
+            TestItem.FirstName = "John";
+            TestItem.LastName = "Smith";
+            TestItem.PhoneNo = "+444656446641";
+            TestItem.CarRegNo = "AF3E4E";
+            // Add the item to the test list
+            TestList.Add(TestItem);
+            // Assign the test variable into the real one
+            AllCustomers.CustomerList = TestList;
+            // Check whether they match
+            Assert.AreEqual(AllCustomers.CustomerList, TestList);
+        }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            // New instance of clsCustomerCollection class
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            // Create a list named TestList
+            List<clsCustomer> TestList = new List<clsCustomer>();
+            // New instance of clsCustomer class to create the item of test data
+            clsCustomer TestItem = new clsCustomer();
+            // Set its properties
+            TestItem.AddressLine1 = "Grange 51a";
+            TestItem.AddressLine2 = "Leicester";
+            TestItem.CustomerID = 1;
+            TestItem.Email = "test@test.com";
+            TestItem.FirstName = "John";
+            TestItem.LastName = "Smith";
+            TestItem.PhoneNo = "+444656446641";
+            TestItem.CarRegNo = "AF3E4E";
+            // Add the item to the test list
+            TestList.Add(TestItem);
+            // Assign the test variable into the real one
+            AllCustomers.CustomerList = TestList;
+            // Check whether they match
+            Assert.AreEqual(AllCustomers.Count, TestList.Count);
         }
 
         //[TestMethod]
