9b58c43edec45663f26ac2e8da1516b31fe7539b
Tomas <P17208375@my365.dmu.ac.uk>
2019-03-08 15:27:55 +0200

    Created testing and methods for filtering by CarRegNo.

--- Test Framework/tstCustomerCollection.cs
+++ Test Framework/tstCustomerCollection.cs
@@ -121,6 +121,131 @@ namespace Test_Framework
         }
 
         [TestMethod]
+        public void ReportByCarRegNoMethodOK()
+        {
+            // New instance of clsCustomerCollection class
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            // New instance of clsCustomerCollection class
+            clsCustomerCollection FilteredCustomers = new clsCustomerCollection();
+            // Apply a blank string (should return all records)
+            FilteredCustomers.ReportByCarRegNo("");
+            // Check whether they match
+            Assert.AreEqual(AllCustomers.Count, FilteredCustomers.Count);
+        }
+
+        [TestMethod]
+        public void ReportByCarRegNoNoneFound()
+        {
+            // New instance of clsCustomerCollection class
+            clsCustomerCollection FilteredCustomers = new clsCustomerCollection();
+            // Apply a car registration number that doesn't exist
+            FilteredCustomers.ReportByCarRegNo("xxxxxx");
+            // Check whether they match
+            Assert.AreEqual(0, FilteredCustomers.Count);
+        }
+
+        [TestMethod]
+        public void ReportByCarRegNoTestDataFound()
+        {
+            // New instance of clsCustomerCollection class
+            clsCustomerCollection FilteredCustomers = new clsCustomerCollection();
+            // New variable for recording if the data is OK (we assume it is)
+            Boolean OK = true;
+            // Apply a car registration number that doesn't exist
+            FilteredCustomers.ReportByCarRegNo("Test C");
+            // Check that the correct number of records are found
+            if (FilteredCustomers.Count == 2)
+            {
+                // Check that the first record is ID 2
+                if (FilteredCustomers.CustomerList[0].CustomerID != 2)
+                {
+                    OK = false;
+                }
+                // Check that the second record is ID 23
+                if (FilteredCustomers.CustomerList[1].CustomerID != 23)
+                {
+                    OK = false;
+                }
+                // Check that the first records AddressLine1 data is OK
+                if (FilteredCustomers.CustomerList[0].AddressLine1 != "Test AddressLine1                                 ")
+                {
+                    OK = false;
+                }
+                // Check that the second records AddressLine1 data is OK
+                if (FilteredCustomers.CustomerList[1].AddressLine1 != "Test AddressLine1                                 ")
+                {
+                    OK = false;
+                }
+                // Check that the first records AddressLine2 data is OK
+                if (FilteredCustomers.CustomerList[0].AddressLine2 != "Test AddressLine2                                                                                   ")
+                {
+                    OK = false;
+                }
+                // Check that the second records AddressLine2 data is OK
+                if (FilteredCustomers.CustomerList[1].AddressLine2 != "Test AddressLine2                                                                                   ")
+                {
+                    OK = false;
+                }
+                // Check that the first records Email data is OK
+                if (FilteredCustomers.CustomerList[0].Email != "Test Email                                                  ")
+                {
+                    OK = false;
+                }
+                // Check that the second records Email data is OK
+                if (FilteredCustomers.CustomerList[1].Email != "Test Email                                                  ")
+                {
+                    OK = false;
+                }
+                // Check that the first records FirstName data is OK
+                if (FilteredCustomers.CustomerList[0].FirstName != "Test FirstName           ")
+                {
+                    OK = false;
+                }
+                // Check that the second records FirstName data is OK
+                if (FilteredCustomers.CustomerList[1].FirstName != "Test FirstName           ")
+                {
+                    OK = false;
+                }
+                // Check that the first records LastName data is OK
+                if (FilteredCustomers.CustomerList[0].LastName != "Test LastName                           ")
+                {
+                    OK = false;
+                }
+                // Check that the second records LastName data is OK
+                if (FilteredCustomers.CustomerList[1].LastName != "Test LastName                           ")
+                {
+                    OK = false;
+                }
+                // Check that the first records PhoneNo data is OK
+                if (FilteredCustomers.CustomerList[0].PhoneNo != "+441231442641                 ")
+                {
+                    OK = false;
+                }
+                // Check that the second records PhoneNo data is OK
+                if (FilteredCustomers.CustomerList[1].PhoneNo != "+441231442641                 ")
+                {
+                    OK = false;
+                }
+                // Check that the first records CarRegNo data is OK
+                if (FilteredCustomers.CustomerList[0].CarRegNo != "Test C")
+                {
+                    OK = false;
+                }
+                // Check that the second records CarRegNo data is OK
+                if (FilteredCustomers.CustomerList[1].CarRegNo != "Test C")
+                {
+                    OK = false;
+                }
+            }
+            else
+            {
+                OK = false;
+            }
+            // Check whether it is true
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
         public void ThisCustomerOK()
         {
             // New instance of clsCustomerCollection class
